776df1ee2f506a770b1726d5a207266fc72b2256
beau-augusto <beausalgado@gmail.com>
2022-10-07 01:32:12 -0300

    String Reverse Solution

---
+++ coderbyte/string_reverse.js
@@ -0,0 +1,12 @@
+function FirstReverse(str) { 
+
+    let reverse = "";
+ for (i = str.length - 1; i >= 0; i--) {
+ 
+    reverse += str[i]
+ }
+   return reverse; 
+ 
+ }
+    
+ console.log(FirstReverse("coderbyte"));
